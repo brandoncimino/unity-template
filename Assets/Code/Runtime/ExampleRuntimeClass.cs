4746443ec067caef2a963b1680b35f440415915d
brandoncimino <brandon.cimino@gmail.com>
2021-04-17 00:04:04 -0500

    - Revamped folder structure - Added `.adoc` files to the different folders, explaining what they do

---
+++ Assets/Code/Runtime/ExampleRuntimeClass.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Code.Runtime {
+    public class ExampleRuntimeClass {
+        void stuff() {
+            UnityEngine.Random.Range(5, 5);
+            new System.Random(5);
+            UnityEngine.JsonUtility.FromJson<int>("");
+            // Newtonsoft.Json.
+        }
+    }
+}
